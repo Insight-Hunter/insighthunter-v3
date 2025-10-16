4a30b0cadd5537975d7e28b7cf3852fdfd3daf22
James Michael Hunter Turner <michael@turneratlanta.com>
2025-10-16 16:41:14 -0400

    Fix CORS headers syntax in index-cloudflare.js

--- workers/auth/index-cloudflare.js
+++ workers/auth/index-cloudflare.js
@@ -3,13 +3,14 @@
 
 export default {
 async fetch(request, env, ctx) {
+
 // CORS headers
 const corsHeaders = {
-‘Access-Control-Allow-Origin’: ‘*’,
-‘Access-Control-Allow-Methods’: ‘GET, POST, PUT, DELETE, OPTIONS’,
-‘Access-Control-Allow-Headers’: ‘Content-Type, Authorization’,
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization'
 };
-
+  
 ```
 if (request.method === 'OPTIONS') {
   return new Response(null, { headers: corsHeaders });
