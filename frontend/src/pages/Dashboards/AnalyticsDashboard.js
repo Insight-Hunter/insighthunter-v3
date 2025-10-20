b9b0eb91ceb096c540c4d5178f64722fff11a3f3
James Michael Hunter Turner <michael@turneratlanta.com>
2025-10-20 17:02:52 -0400

    Update and rename frontend/src/pages/AnalyticsDashboard.js to frontend/src/pages/Dashboards/AnalyticsDashboard.js

--- frontend/src/pages/Dashboards/AnalyticsDashboard.js
+++ frontend/src/pages/Dashboards/AnalyticsDashboard.js
@@ -1,4 +1,4 @@
-// frontend/src/pages/AnalyticsDashboard.js
+// frontend/src/pages/AnalyticsDashboard.jsx
 // Complete dashboard that uses the analytics Worker
 
 import React, { useState, useEffect } from 'react';
