2a09809c40ecd7bdce1e2215e020d9eeadd414c4
James Michael Hunter Turner <michael@turneratlanta.com>
2025-10-29 17:21:45 -0400

    Update and rename backend/middlewares/validation.ts to backend/src/middlewares/validation.ts

--- backend/src/middlewares/validation.ts
+++ backend/src/middlewares/validation.ts
@@ -1,5 +1,5 @@
 import Joi from "joi";
-import { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 
 export const validateSchema = (schema: Joi.ObjectSchema) => {
   return (req: Request, res: Response, next: NextFunction) => {
